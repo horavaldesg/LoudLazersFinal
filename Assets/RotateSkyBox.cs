21399ec21787ca3e8aae0348c2b2d85ae5994b60
Horacio Valdes <horavaldesg@gmail.com>
2021-04-29 20:38:09 -0400

    Update Fix

---
+++ Assets/RotateSkyBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkyBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time);
+    }
+}
